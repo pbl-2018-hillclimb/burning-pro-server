e5d95c28380893975c7aee123ce2c55ca6866a1a
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-06-13 14:57:00 +0900

    Initialize logger

--- burning-pro-server/src/main.rs
+++ burning-pro-server/src/main.rs
@@ -1,4 +1,37 @@
 //! Burning-pro server.
 #![warn(missing_docs)]
 
-fn main() {}
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+
+use std::env;
+
+
+/// Setup global logger.
+fn setup_logger() {
+    const DEFAULT_LOG_LEVEL: &str = "trace";
+    let underscored_name = env!("CARGO_PKG_NAME").replace('-', "_");
+    let defval = format!("{}={}", underscored_name, DEFAULT_LOG_LEVEL);
+
+    let newval = match env::var("RUST_LOG") {
+        Ok(v) => format!("{},{}", defval, v),
+        Err(_) => defval,
+    };
+    env::set_var("RUST_LOG", &newval);
+
+    pretty_env_logger::init();
+
+    trace!("RUST_LOG={}", newval);
+}
+
+
+fn main() {
+    setup_logger();
+
+    info!(
+        "{}, version {}",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION")
+    );
+}
