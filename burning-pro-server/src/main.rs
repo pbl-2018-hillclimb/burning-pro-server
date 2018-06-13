f8126cc4366d8b810f377919c6748a7253ce81c4
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-06-13 15:18:22 +0900

    Implement primitive server

--- burning-pro-server/src/main.rs
+++ burning-pro-server/src/main.rs
@@ -1,11 +1,14 @@
 //! Burning-pro server.
 #![warn(missing_docs)]
 
+extern crate actix_web;
 #[macro_use]
 extern crate log;
 extern crate pretty_env_logger;
 
 use std::env;
+
+use actix_web::{server, App, HttpRequest};
 
 
 /// Setup global logger.
@@ -26,6 +29,13 @@ fn setup_logger() {
 }
 
 
+fn fire(req: HttpRequest) -> &'static str {
+    debug!("request for `fire()`: {:?}", req);
+    // Fire.
+    "\u{1F525}"
+}
+
+
 fn main() {
     setup_logger();
 
@@ -34,4 +44,10 @@ fn main() {
         env!("CARGO_PKG_NAME"),
         env!("CARGO_PKG_VERSION")
     );
+
+    info!("starting server...");
+    server::new(|| App::new().resource("/", |r| r.f(fire)))
+        .bind("127.0.0.1:8080")
+        .expect("Failed to bind 127.0.0.1:8080")
+        .run();
 }
