2e2ca94a01d63366e45894f23eea8c8220cd65f7
Arimichi Matsumura <12750219+popon-01@users.noreply.github.com>
2018-07-14 18:51:54 +0900

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+# usage : ssh -t [remote host] sudo [path to script]
+
+image_name="burning-pro-server-image"
+container_name="burning-pro-server"
+volume_host="/srv/burning-pro-server"
+volume_container="/data"
+run_opt="-itd --rm -v ${volume_host}/db:${volume_container}/db -p 8080:8080"
+
+# move to repository and get latest files
+cd "$(dirname "$(readlink -f "$0")")"
+git pull --ff-only
+
+# build
+docker build -t ${image_name} .
+
+# stop old container (automatically removed by --rm option)
+for container_id in $(docker ps -aq --filter "ancestor=${image_name}");
+do
+    docker stop ${container_id}
+done
+
+# restart
+docker run ${run_opt} --name ${container_name} ${image_name}
