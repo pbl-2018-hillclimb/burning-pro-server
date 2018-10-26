8f683ecb00740dde7ec8c71914a086bf2a5e1344
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-10-26 18:43:08 +0900

    Use outer attribute to protect it against `schema.rs` auto-generation
    `schema.rs` is generated by `diesel migration run`, so this file itself
    should not contain additional attribute.

--- burning-pro-server/src/lib.rs
+++ burning-pro-server/src/lib.rs
@@ -25,4 +25,7 @@ pub mod app;
 pub mod db;
 pub mod good_phrase;
 pub(crate) mod models;
+// Temporal silence until diesel-1.4.
+// See <https://github.com/diesel-rs/diesel/issues/1785#issuecomment-422579609>.
+#[allow(proc_macro_derive_resolution_fallback)]
 mod schema;
--- burning-pro-server/src/schema.rs
+++ burning-pro-server/src/schema.rs
@@ -1,7 +1,3 @@
-// Temporal silence until diesel-1.4.
-// See <https://github.com/diesel-rs/diesel/issues/1785#issuecomment-422579609>.
-#![allow(proc_macro_derive_resolution_fallback)]
-
 table! {
     good_phrase_tags (good_phrase_tag_id) {
         good_phrase_tag_id -> Integer,
