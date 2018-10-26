072b382a409347e2e0353ca9cc41ffad504e6a43
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-10-26 14:39:08 +0900

    Silence diesel warnings which would be fixed in diesel-1.4
    This is temporary workaround.
    See <https://github.com/diesel-rs/diesel/issues/1785#issuecomment-422579609>.

--- burning-pro-server/src/models/update.rs
+++ burning-pro-server/src/models/update.rs
@@ -1,5 +1,9 @@
 //! Data types insertable to DB.
 
+// Temporal silence until diesel-1.4.
+// See <https://github.com/diesel-rs/diesel/issues/1785#issuecomment-422579609>.
+#![allow(proc_macro_derive_resolution_fallback)]
+
 use chrono::NaiveDateTime;
 
 use schema::*;
